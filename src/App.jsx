6233858495078ad59b52506c5477464f22a81b3a
Varna <varnamalayil123@gmail.com>
2025-09-21 12:13:16 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { Routes, Route } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import ShowcasePage from "./pages/ShowcasePage"
-import ContactPage from "./pages/ContactPage"
-import NewsPage from "./pages/NewsPage"
-import CategoryPage from "./pages/CategoryPage"
+// import { Routes, Route } from "react-router-dom"
+// import HomePage from "./pages/HomePage"
+// import ShowcasePage from "./pages/ShowcasePage"
+// import ContactPage from "./pages/ContactPage"
+// import NewsPage from "./pages/NewsPage"
+// import CategoryPage from "./pages/CategoryPage"
 
-function App() {
-  return (
-    <div className="min-h-screen bg-black text-white">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/showcase" element={<ShowcasePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/category/:slug" element={<CategoryPage />} />
-      </Routes>
-    </div>
-  )
-}
+// function App() {
+//   return (
+//     <div className="min-h-screen bg-black text-white">
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/showcase" element={<ShowcasePage />} />
+//         <Route path="/contact" element={<ContactPage />} />
+//         <Route path="/news" element={<NewsPage />} />
+//         <Route path="/category/:slug" element={<CategoryPage />} />
+//       </Routes>
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
